ee97bf170d66e0b0f90d00ed927cbf7ff46c46ed
OUT-Aliev-KA <out-aliev-ka@cab-wsm-0000299.sigma.sbrf.ru>
2019-04-30 20:33:44 +0300

    added unity documentaton section(working on webrequest), finished first version of quiz game in tutorials section

---
+++ unity/learn.unity.com/tutorials/scripting/Quiz Game/Assets/Scripts/DataController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+    public RoundData[] allRoundData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject); // when moved to new scenes gets preserved
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity/learn.unity.com/tutorials/scripting/Quiz Game/Assets/Scripts/SimpleObjectPool.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectPool : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    public GameObject GetObject()
+    {
+        GameObject spawnedGameObject;
+
+        if (inactiveInstances.Count > 0)
+        {
+            spawnedGameObject = inactiveInstances.Pop();
+        }
+        else
+        {
+            spawnedGameObject = (GameObject)GameObject.Instantiate(prefab);
+            PooledObject pooledObject = spawnedGameObject.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+        }
+
+        spawnedGameObject.SetActive(true);
+        return spawnedGameObject;
+    }
+
+    public void ReturnObject(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+        if(pooledObject != null && pooledObject.pool == this)
+        {
+            toReturn.SetActive(false);
+
+            inactiveInstances.Push(toReturn);
+        }
+        else // error case
+        {
+            Destroy(toReturn);
+        }
+    }
+}
+
+public class PooledObject : MonoBehaviour
+{
+    public SimpleObjectPool pool;
+}
