a4fbee0159760c1910c6128649efb7774848e0fd
OUT-Aliev-KA <out-aliev-ka@cab-wsm-0000299.sigma.sbrf.ru>
2019-05-08 18:57:29 +0300

    added Sandbox and beginener gameplay scripting notes in unity tutorials

---
+++ unity/learn.unity.com/tutorials/scripting/beginner_gameplay_scripting/Sandbox/Assets/Scripts/GetAxisExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAxisExample : MonoBehaviour
+{
+    public float range;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * range;
+        float yPos = v * range;
+
+        transform.position = new Vector3(xPos, yPos, 0);
+    }
+}
---
+++ unity/learn.unity.com/tutorials/scripting/beginner_gameplay_scripting/Sandbox/Assets/Scripts/InvokeExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeExample : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        //Invoke("SpawnObject", 2);
+        InvokeRepeating("SpawnObject", 2, 1);
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(5, 0, 0), Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CancelInvoke();
+        }
+    }
+}
